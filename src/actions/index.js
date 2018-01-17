187156610f092dabf96caf180c879c8f5e25ac2d
Andrew <andrew.vinocur@gmail.com>
2018-01-16 16:55:11 -0800

    finishes action creater fetch_Weater that will send an api request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,22 @@
-import KEY from './config';
+import axios from 'axios';
 
+import KEY from './config';
 const API_KEY = KEY;
+// ES6 template string used below
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+// By declaring a variable that is equal to a string to be used on the
+// type of the Action that is returned, we avoid issues of mistyping, etc.
+// as this variable is exported from here to be available for use in reducers
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeater(city) {
+  //using us to make country code static
+  const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
