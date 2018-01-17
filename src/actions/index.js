fba4b0b3411ccfa35a652865a4323e24cf914c73
Andrew <andrew.vinocur@gmail.com>
2018-01-16 17:20:01 -0800

    fixes url from sample to api

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import KEY from '../config';
 const API_KEY = KEY;
 // ES6 template string used below
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 // By declaring a variable that is equal to a string to be used on the
 // type of the Action that is returned, we avoid issues of mistyping, etc.
