0eb21a465c8413a3d527b8828f412f87385dbfef
Andrew <andrew.vinocur@gmail.com>
2018-01-17 19:01:40 -0800

    changes color and data to utilizize props and add customization

--- src/components/chart.js
+++ src/components/chart.js
@@ -5,8 +5,8 @@ import { Sparklines, SparklinesLine } from 'react-sparklines';
 export default (props) => {
   return (
     <div>
-      <Sparklines height={120} width={180} data={temps}>
-        <SparklinesLine color="blue" />
+      <Sparklines height={120} width={180} data={props.data}>
+        <SparklinesLine color={props.color} />
       </Sparklines>
     </div>
   );
