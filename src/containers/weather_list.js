674322759f8dee6125625aa328bdc9ef6ed37a9e
Andrew <andrew.vinocur@gmail.com>
2018-01-17 18:58:43 -0800

    pulls out sparklines imports and display for use in chart component

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -11,9 +10,7 @@ class WeatherList extends Component {
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="blue" />
-          </Sparklines>
+
         </td>
       </tr>
     );
