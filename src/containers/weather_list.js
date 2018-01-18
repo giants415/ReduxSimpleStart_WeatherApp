654a79359809d6bfec1cce29b88a4f9eb16ba801
Andrew <andrew.vinocur@gmail.com>
2018-01-17 18:13:33 -0800

    adds more detail to table. connects weather list container to redux

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,17 +1,32 @@
- import React, { Component } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
- export default class WeatherList extends Component {
-   render() {
-    return(
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>City</th>
-          </tr>
-        </thead>
-        <tbody>
-        </tbody>
-      </table>
-    );
-   }
+class WeatherList extends Component {
+ render() {
+  return(
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th>City</th>
+          <th>Temperature</th>
+          <th>Pressure</th>
+          <th>Humidity</th>
+        </tr>
+      </thead>
+      <tbody>
+
+      </tbody>
+    </table>
+  );
  }
+}
+
+function mapStateToProps({ weather }) {
+  // above, weather is pulled off the larger state object (es6 syntax)
+  // below, due to the above, the key and value would be the same
+  // (ie. { weather: weather })but, since they are the same, can be refactored
+  // to just be what is below
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
