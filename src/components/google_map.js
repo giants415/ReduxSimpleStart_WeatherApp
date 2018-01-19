5d9adf13f5e9a0615a8dbb5ac95d55530f823fdf
Andrew <andrew.vinocur@gmail.com>
2018-01-18 18:23:08 -0800

    small note going forward concerning 3rd party libraries and react

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 class GoogleMap extends Component {
+  // must use componentDidMount when working with 3rd party libraries
+  // that don't "understand" how to work with React's render method
   componentDidMount() {
     new google.maps.Map(this.refs.map, {
       zoom: 12,
