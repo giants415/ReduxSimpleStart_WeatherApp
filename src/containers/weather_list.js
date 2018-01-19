d85061a65d0ed11b9af74476ffd488f1bdf446f6
Andrew <andrew.vinocur@gmail.com>
2018-01-18 18:28:05 -0800

    imports and implements googlemap component. Also, uses es6 to access lon and lat data

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -8,10 +9,13 @@ class WeatherList extends Component {
     const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp * (9/5) - 459.67);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    // below says: access the coord object, grab the lon and lat properties
+    // and then assign them to a "new" consts with names lon and lat
+    const { lon, lat } = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap /></td>
         <td><Chart data={temps} color="red" units="F"/></td>
         <td><Chart data={pressures} color="green" units="hPa"/></td>
         <td><Chart data={humidities} color="blue" units="%"/></td>
